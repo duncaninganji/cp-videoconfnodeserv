bca4c4400e1a4044e30a43fe175bdbb03cbfa0d4
Duncan Inganji <duncan.inganji@berkeley.edu>
2020-10-23 02:59:46 -0700

    CRUD for tokens API

---
+++ src/controllers/tokens.js
@@ -0,0 +1,124 @@
+import moment from 'moment';
+import Firestore from '@google-cloud/firestore';
+import { TOKENS_COLLECTION_ID } from '../constants.js';
+
+// create a new client
+const firestore = new Firestore();
+
+export async function addAccessToken(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(404).send("Request object missing body")
+  }
+
+  const { userId, token } = req.body
+  const tokenObj = {
+    userId,
+    token,
+    dateModified: moment.utc().toDate(),
+    revoked: false
+  }
+  // api call to firestore to add token to database
+  firestore
+    .collection(TOKENS_COLLECTION_ID)
+    .doc(userId)
+    .set(tokenObj)
+    .then(() => {  
+      console.log(`Token for user with id ${userId} added`)
+      return res.status(200).send(`Token for user with id ${userId} added`)
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(404).send('Error adding token to database')
+    });
+}
+
+
+export async function getAccessToken(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing id query")
+    return res.status(404).send("Request object missing id query")
+  }
+
+  const userId = req.body.userId || ''
+
+  firestore
+    .collection(TOKENS_COLLECTION_ID)
+    .doc(userId)
+    .get()
+    .then(token => {
+      token = token.data()
+      if (token && !token.revoked) {
+        console.log(`Token for user with id ${userId} found\n`, token)
+        req.body.token = token
+        return next()
+      }
+
+      console.log(`Token for user with id ${userId} does not exist`)
+      return res.status(404).send(`Token for user with id ${tokenId} does not exist`)
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(404).send("Error fetching token from database")
+    })
+}
+
+export function checkIfTokenExpired(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(404).send("Request object missing body")
+  }
+
+  const { tokenObj } = req.body.token
+  req.body.expired = moment.utc().diff(tokenObj.dateModified, 'seconds') > tokenObj.token.expires_in;
+  return next();
+}
+
+export async function updateAccessToken(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(404).send("Request object missing body")
+  }
+
+  if (!req.body.expired) return next();
+
+  const userId = req.body.userId
+  // api call to firestore to update token
+  firestore
+    .collection(TOKENS_COLLECTION_ID)
+    .doc(userId)
+    .set(req.body.token)
+    .then(() => {  
+      console.log(`Token for user with id ${userId} updated`)
+      return next()
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(404).send('Error updating token')
+    });
+}
+
+
+export async function deleteAccessToken(req, res) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(404).send("Request object missing body")
+  }
+
+  const userId = req.body.userId
+
+  firestore
+    .collection(TOKENS_COLLECTION_ID)
+    .doc(userId)
+    .update({
+      revoked: true
+    })
+    .then(() => {
+      console.log(`token with ${userId} revoked\n`)
+      return res.status(200).json({'status': 'success'})
+    })
+    .catch(error => {
+      console.error(error)
+      res.status(404).send("Error deleting token")
+    })
+}
---
+++ src/zoomTokens.js
@@ -0,0 +1,104 @@
+import axios from 'axios';
+import moment from 'moment'
+import qs from 'qs';
+import { 
+  ZOOM_AUTH_HEADER, 
+  ZOOM_ACCESS_TOKEN_URL, 
+  ZOOM_REDIRECT_URI 
+} from './constants.js';
+import { ZOOM_REVOKE_ACCESS_URL } from './constants.js';
+
+export async function fetchAccessToken(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(404).send("Request object missing body")
+  }
+
+  const { authCode } = req.body
+
+  const options = {
+    method: 'POST',
+    url: ZOOM_ACCESS_TOKEN_URL,
+    headers: { 'Authorization': ZOOM_AUTH_HEADER},
+    data: qs.stringify({
+        'grant_type': 'authorization_code',
+        'code': authCode,
+        'redirect_uri': ZOOM_REDIRECT_URI
+    })
+  }
+  
+  // api call to zoom to fetch access token
+  axios(options)
+    .then((token) => {
+      req.body.token = JSON.parse(token);
+      return next()
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(404).send('Error fetching access token');
+    })
+}
+
+export async function refreshAccessToken(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(404).send("Request object missing body")
+  }
+
+  if (!req.body.expired) return next();
+
+  const options = {
+    method: 'POST',
+    url: ZOOM_ACCESS_TOKEN_URL,
+    headers: { 'Authorization': ZOOM_AUTH_HEADER},
+    data: qs.stringify({
+        'grant_type': 'refresh_token',
+        'refresh_token': req.body.token.refresh_token
+    })
+  }
+  
+  // api call to zoom to refresh access token
+  axios(options)
+    .then((token) => {
+      const newToken = {
+        userId: req.body.userId,
+        tokenObj: JSON.parse(token),
+        dateModified: moment.utc().toDate(),
+        revoked: false
+      }
+      req.body.token = newToken
+      return next()
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(404).send('Error refreshing access token');
+    })
+}
+
+export async function revokeAccessToken(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(404).send("Request object missing body")
+  }
+
+  const { userId, tokenObj } = req.body.token
+  const options = {
+    method: 'POST',
+    url: ZOOM_REVOKE_ACCESS_URL,
+    headers: { 'Authorization': ZOOM_AUTH_HEADER},
+    data: qs.stringify({
+        'token': tokenObj.access_token
+    })
+  }
+  
+  // api call to zoom to refresh access token
+  axios(options)
+    .then(() => {
+      console.log(`Access token for user with id ${userId} revoked`)
+      return next()
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(404).send('Error revoking access token');
+    })
+}
