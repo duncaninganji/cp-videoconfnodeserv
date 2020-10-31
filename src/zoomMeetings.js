211545c14ef0a8e26af30ebb557c43919ddfa86b
Duncan Inganji <duncan.inganji@berkeley.edu>
2020-10-30 18:00:32 -0700

    CRUD api for zoom meetings and webinars

---
+++ src/zoomMeetings.js
@@ -0,0 +1,255 @@
+import axios from 'axios';
+import qs from 'qs';
+
+
+// req object should have the zoomId as well as the userId
+// token should have been added to the body of the request object 
+// before this by the fetchAccessToken function
+export async function createMeeting(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, token } = req.body
+  const url = `https://api.zoom.us/v2/users/${zoomId}/meetings`
+  const headers = {
+    'Content-Transfer-Encoding': 'application/json',
+    'Authorization': `Bearer ${token}`
+  }
+  // TODO: Fill in default values for the body
+  const body = {}
+
+  const options = {
+    method: 'POST',
+    url,
+    body,
+  }
+
+  axios(options)
+    .then(meetingObj => {
+      console.log(JSON.stringify(meetingObj));
+      return res.status(200).json(meetingObj.response.data);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error creating meeting for user with zoom id ${zoomId}`)
+    })
+
+}
+
+export async function getMeeting(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, meetingId, token } = req.body
+  const url = `https://api.zoom.us/v2/meetings/${meetingId}`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'GET',
+    url,
+  }
+
+  axios(options)
+    .then(meetingObj => {
+      console.log(JSON.stringify(meetingObj));
+      return res.status(200).json(meetingObj.response.data);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting meeting with id ${meetingId}`)
+    })
+}
+
+// could paginate the output if we wanted to
+export async function getAllMeetingsForUser(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, token } = req.body
+  const url = `https://api.zoom.us/v2/users/${zoomId}/meetings`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'GET',
+    headers,
+    url,
+  }
+
+  axios(options)
+    .then(meetingsList => {
+      console.log(JSON.stringify(meetingsList));
+      return res.status(200).json(meetingsList.response.data);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting meetings for user with zoom id ${zoomId}`)
+    })
+} 
+
+export async function updateMeeting(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, meetingId, token, updateObj } = req.body
+  const url = `https://api.zoom.us/v2/meetings/${meetingId}`
+  const headers = {
+    'Content-Transfer-Encoding': 'application/json',
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'PATCH',
+    body: updateObj,
+    headers,
+    url
+  }
+
+  axios(options)
+    .then(() => {
+      console.log(`Meeting with id${meetingId} successfully updated`);
+      return res.status(200).send(`Meeting with id${meetingId} successfully updated`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting updating meeting with id ${meetingId}`)
+    })
+}
+
+export async function updateMeetingStatus(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, meetingId, token, updateObj } = req.body
+  const url = `https://api.zoom.us/v2/meetings/${meetingId}`
+  const headers = {
+    'Content-Transfer-Encoding': 'application/json',
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'PATCH',
+    body: updateObj,
+    headers,
+    url
+  }
+
+  axios(options)
+    .then(() => {
+      console.log(`Meeting with id${meetingId} successfully updated`);
+      return res.status(200).send(`Meeting with id${meetingId} successfully updated`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting updating meeting with id ${meetingId}`)
+    })
+}
+
+export async function deleteMeeting(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, meetingId, token } = req.body
+  const url = `https://api.zoom.us/v2/meetings/${meetingId}`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'DELETE',
+    params: qs.stringify({
+      'schedule_for_reminder': true,
+      'cancel_meeting_reminder': true
+    }),
+    headers,
+    url
+  }
+
+  axios(options)
+    .then(() => {
+      console.log(`Meeting with id${meetingId} successfully deleted`);
+      return res.status(200).send(`Meeting with id${meetingId} successfully deleted`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting deleting meeting with id ${meetingId}`)
+    })
+}
+
+export async function listMeetingRegistrants(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, meetingId, token } = req.body
+  const url = `https://api.zoom.us/v2/meetings/${meetingId}/registrants`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'GET',
+    params: qs.stringify({
+      'status': 'approved'
+    }),
+    headers,
+    url,
+  }
+
+  axios(options)
+    .then(registrantsList => {
+      console.log(JSON.stringify(registrantsList));
+      return res.status(200).json(registrantsList.response.data);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting registrants for meeting with id ${meetingId}`)
+    })
+}
+
+export async function updateMeetingRegistrants(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, meetingId, token, updateObj } = req.body
+  const url = `https://api.zoom.us/v2/meetings/${meetingId}/registrants/status`
+  const headers = {
+    'Content-Transfer-Encoding': 'application/json',
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'PUT',
+    body: updateObj,
+    headers,
+    url,
+  }
+
+  axios(options)
+    .then(() => {
+      console.log(`Registrants for meeting with id ${meetingId} updated`);
+      return res.status(200).send(`Registrants for meeting with id ${meetingId} updated`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error updating registrants for meeting with id ${meetingId}`)
+    })
+}
---
+++ src/zoomWebinars.js
@@ -0,0 +1,408 @@
+import axios from 'axios';
+import qs from 'qs';
+
+
+// req object should have the zoomId as well as the userId
+// token should have been added to the body of the request object 
+// before this by the fetchAccessToken function
+export async function createWebinar(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, token } = req.body
+  const url = `https://api.zoom.us/v2/users/${zoomId}/webinars`
+  const headers = {
+    'Content-Transfer-Encoding': 'application/json',
+    'Authorization': `Bearer ${token}`
+  }
+  // TODO: Fill in default values for the body
+  const body = {}
+
+  const options = {
+    method: 'POST',
+    url,
+    body,
+  }
+
+  axios(options)
+    .then(webinarObj => {
+      console.log(JSON.stringify(webinarObj));
+      return res.status(200).json(webinarObj.response.data);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error creating webinar for user with zoom id ${zoomId}`)
+    })
+
+}
+
+export async function getWebinar(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, webinarId, token } = req.body
+  const url = `https://api.zoom.us/v2/webinars/${webinarId}`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'GET',
+    url,
+  }
+
+  axios(options)
+    .then(webinarObj => {
+      console.log(JSON.stringify(webinarObj));
+      return res.status(200).json(webinarObj.response.data);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting webinar with id ${webinarId}`)
+    })
+}
+
+// could paginate the output if we wanted to
+export async function getAllWebinarsForUser(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, token } = req.body
+  const url = `https://api.zoom.us/v2/users/${zoomId}/webinars`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'GET',
+    headers,
+    url,
+  }
+
+  axios(options)
+    .then(webinarsList => {
+      console.log(JSON.stringify(webinarsList));
+      return res.status(200).json(webinarsList.response.data);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting webinars for user with zoom id ${zoomId}`)
+    })
+} 
+
+export async function updateWebinar(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, webinarId, token, updateObj } = req.body
+  const url = `https://api.zoom.us/v2/webinars/${webinarId}`
+  const headers = {
+    'Content-Transfer-Encoding': 'application/json',
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'PATCH',
+    body: updateObj,
+    headers,
+    url
+  }
+
+  axios(options)
+    .then(() => {
+      console.log(`Webinar with id${webinarId} successfully updated`);
+      return res.status(200).send(`Webinar with id${webinarId} successfully updated`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting updating webinar with id ${webinarId}`)
+    })
+}
+
+export async function updateWebinarStatus(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, webinarId, token, updateObj } = req.body
+  const url = `https://api.zoom.us/v2/webinars/${webinarId}`
+  const headers = {
+    'Content-Transfer-Encoding': 'application/json',
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'PATCH',
+    body: updateObj,
+    headers,
+    url
+  }
+  
+  axios(options)
+    .then(() => {
+      console.log(`Webinar with id${webinarId} successfully updated`);
+      return res.status(200).send(`Webinar with id${webinarId} successfully updated`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting updating webinar with id ${webinarId}`)
+    })
+}
+
+export async function deleteWebinar(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, webinarId, token } = req.body
+  const url = `https://api.zoom.us/v2/webinars/${webinarId}`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'DELETE',
+    params: qs.stringify({
+      'cancel_webinar_reminder': true
+    }),
+    headers,
+    url
+  }
+
+  axios(options)
+    .then(() => {
+      console.log(`Webinar with id${webinarId} successfully deleted`);
+      return res.status(200).send(`Webinar with id${webinarId} successfully deleted`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting deleting webinar with id ${webinarId}`)
+    })
+}
+
+export async function listWebinarPanelists(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { zoomId, userId, webinarId, token } = req.body
+  const url = `https://api.zoom.us/v2/webinars/${webinarId}/panelists`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'GET',
+    headers,
+    url,
+  }
+
+  axios(options)
+    .then(panelists => {
+      console.log(JSON.stringify(panelists));
+      return res.status(200).json(panelists.response.data);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting panelists for webinar with id ${webinarId}`)
+    })
+}
+
+export async function addPanelists(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { webinarId, token, panelists} = req.body
+  const url = `https://api.zoom.us/v2/webinars/${webinarId}/panelists`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'POST',
+    body: panelists,
+    headers,
+    url,
+  }
+
+  axios(options)
+    .then(() => {
+      console.log(`Panelists successfully added to ${webinarId}`);
+      return res.status(200).send(`Panelists successfully added to ${webinarId}`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error adding panelists for webinar with id ${webinarId}`)
+    })
+}
+
+export async function removePanelist(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { webinarId, token, panelistId} = req.body
+  const url = `https://api.zoom.us/v2/webinars/${webinarId}/panelists/${panelistId}`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'DELETE',
+    body: panelists,
+    headers,
+    url,
+  }
+
+  // returns 200 if webinar plan subscription is missing
+  // returns 204 if panelist is successfully deleted
+  axios(options)
+    .then(() => {
+      console.log(`Panelist with id ${panelistId} successfully deleted from ${webinarId}`);
+      return res.status(200).send(`Panelist with id ${panelistId} successfully deleted from ${webinarId}`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error deleting panelist with id ${panelistId} from webinar with id ${webinarId}`)
+    })
+}
+
+export async function removeAllPanelists(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { webinarId, token} = req.body
+  const url = `https://api.zoom.us/v2/webinars/${webinarId}/panelists`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'DELETE',
+    body: panelists,
+    headers,
+    url,
+  }
+
+  // returns 200 if webinar plan subscription is missing
+  // returns 204 if panelist is successfully deleted
+  axios(options)
+    .then(() => {
+      console.log(`All Panelists successfully deleted from webinar with id ${webinarId}`);
+      return res.status(200).send(`All Panelists successfully deleted from webinar with id ${webinarId}`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error deleting panelists from webinar with id ${webinarId}`)
+    })
+}
+
+export async function listWebinarRegistrants(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { webinarId, token} = req.body
+  const url = `https://api.zoom.us/v2/webinars/${webinarId}/registrants`
+  const headers = {
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'GET',
+    params: qs.stringify({
+      'status': 'approved'
+    }),
+    headers,
+    url,
+  }
+
+  axios(options)
+    .then(registrantsList => {
+      console.log(JSON.stringify(registrantsList));
+      return res.status(200).json(registrantsList.response.data);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error getting registrants for webinar with id ${webinarId}`)
+    })
+}
+
+export async function addWebinarRegistrants(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { webinarId, token, registrantObj} = req.body
+  const url = `https://api.zoom.us/v2/webinars/${webinarId}/registrants`
+  const headers = {
+    'Content-Transfer-Encoding': 'application/json',
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'POST',
+    body: registrantObj,
+    headers,
+    url,
+  }
+
+  axios(options)
+    .then(() => {
+      console.log(`Registrant added to webinar with id ${webinarId}`);
+      return res.status(200).send(`Registrant added to webinar with id ${webinarId}`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error adding registrant to webinar with id ${webinarId}`)
+    })
+}
+
+export async function updateWebinarRegistrants(req, res, next) {
+  if (!req.body) {
+    console.log("Request object missing body")
+    return res.status(400).send("Request object missing body")
+  }
+
+  const { webinarId, token, updateObj} = req.body
+  const url = `https://api.zoom.us/v2/webinars/${webinarId}/registrants/status`
+  const headers = {
+    'Content-Transfer-Encoding': 'application/json',
+    'Authorization': `Bearer ${token}`
+  }
+
+  const options = {
+    method: 'PUT',
+    body: updateObj,
+    headers,
+    url,
+  }
+
+  axios(options)
+    .then(() => {
+      console.log(`Updated registrants for webinar with id ${webinarId}`);
+      return res.status(200).send(`Updated registrants for webinar with id ${webinarId}`);
+    })
+    .catch(error => {
+      console.error(error)
+      return res.status(error.response.status || 404).send(`Error adding registrant to webinar with id ${webinarId}`)
+    })
+}
